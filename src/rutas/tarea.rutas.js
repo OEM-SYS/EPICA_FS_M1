43cf178e134515c7c32588d7c7689b9942caa2d8
OEM-SYS <eduardomarceloolivares@gmail.com>
2023-09-13 11:49:18 -0300

    arreglado el problema de las rutas, ahora funciona el CRUD :-)

--- src/rutas/tarea.rutas.js
+++ src/rutas/tarea.rutas.js
@@ -1,18 +1,23 @@
 import {Router} from "express";
 import { controladorCrearTarea, controladorEliminarTarea, controladorModificarTarea, controladorObtenerTareas, controladorVista } from "../controladores/tarea.controladores.js";
+import { validador } from "../middlewares/validador.js";
+import { modeloTarea } from "../modelos/tareas.js";
 
 const tareaEnrutador= Router();//este es el enrutador utilizado en aplicacion.js
 
 //enpoint para obtener todas las tareas
-tareaEnrutador.get('api/tareas', controladorObtenerTareas);
+tareaEnrutador.get('/api/tareas', controladorObtenerTareas);
 
 //enpoint para crear una tarea
-tareaEnrutador.post('api/tareas', controladorCrearTarea);
+tareaEnrutador.post('/api/tareas', controladorCrearTarea);
 
 //enpoint para modificar una tarea
-tareaEnrutador.put('api/:ID', controladorModificarTarea);
+tareaEnrutador.put('/api/tareas/:ID', controladorModificarTarea);
 
 //enpoint para eliminar una tarea
-tareaEnrutador.delete('api/:ID', controladorEliminarTarea);
+tareaEnrutador.delete('/api/tareas/:ID', controladorEliminarTarea);
+
+//ruta para la vista
+tareaEnrutador.get('/tareas', controladorVista);
 
 export {tareaEnrutador};
